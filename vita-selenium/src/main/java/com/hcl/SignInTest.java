9e0612414743461298f87e11d6a60fc245bbc613
acoe-hcl <acoe.hcl@gmail.com>
2023-10-27 04:59:26 -0700

    Commit all changes including additions

---
+++ vita-selenium/src/main/java/com/hcl/SignInTest.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SignInTest {
+    public static void main(String[] args) {
+        // Set the path of the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        // Create a new instance of the ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to the LinkedIn homepage
+        driver.get("https://www.linkedin.com");
+
+        // Find the username field and enter the username
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys("acoe.hcl@gmail.com");
+
+        // Find the password field and enter the password
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("hcltech@123");
+
+        // Find the sign in button and click it
+        WebElement signInButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        signInButton.click();
+
+        // Verify that the user is on the LinkedIn feed page
+        String expectedUrl = "https://www.linkedin.com/feed/";
+        String actualUrl = driver.getCurrentUrl();
+        if (actualUrl.equals(expectedUrl)) {
+            System.out.println("Test passed!");
+        } else {
+            System.out.println("Test failed");
+        }
+
+        // Close the browser
+        driver.quit();
+    }
+}
+									
