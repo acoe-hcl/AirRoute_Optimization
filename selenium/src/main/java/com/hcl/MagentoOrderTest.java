403e2e1ec02ba2af38fba632d3b498c68582a74a
digitalcoe <ashwinkumar.j@hcl.com>
2024-02-20 13:17:27 +0000

    Commit all changes including additions

--- selenium/src/main/java/com/hcl/MagentoOrderTest.java
+++ selenium/src/main/java/com/hcl/MagentoOrderTest.java
@@ -1,77 +1,67 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class MagentoOrderTest {
-
     WebDriver driver;
 
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "path_to_chrome_driver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
-    public void tearDown() {
-        driver.quit();
-    }
-
+    @Test
     public void placeOrderTest() {
-        setUp();
+        // Initialize WebDriver
 
-        // Launch Application
+        // Launch the application
         driver.get("https://magento.softwaretestingboard.com/");
 
-        // Sign In
+        // Sign in
         driver.findElement(By.linkText("Sign In")).click();
         driver.findElement(By.id("email")).sendKeys("autotest567@gmail.com");
         driver.findElement(By.id("pass")).sendKeys("Tester@123");
         driver.findElement(By.id("send2")).click();
 
-        // Navigate to Gear > Bags
+        // Navigate to Bags - Gear
         Actions actions = new Actions(driver);
-        actions.moveToElement(driver.findElement(By.id("gear"))).perform();
+        WebElement gearMenu = driver.findElement(By.xpath("//*[@id='ui-id-5']"));
+        actions.moveToElement(gearMenu).perform();
         driver.findElement(By.linkText("Bags")).click();
 
-        // Select Product and Add to Cart
-        driver.findElement(By.xpath("//img[@alt='Overnight Duffle']")).click();
-        driver.findElement(By.id("product-addtocart-button")).click();
+        // Select Overnight Duffle
+        driver.findElement(By.xpath("//a[@title='Overnight Duffle']")).click();
+        driver.findElement(By.xpath("//span[text()='Add to Cart']")).click();
+
+        // Proceed to Checkout
         driver.findElement(By.linkText("My Cart")).click();
-        driver.findElement(By.id("proceed-checkout")).click();
+        driver.findElement(By.xpath("//span[text()='Proceed to Checkout']")).click();
 
-        // Verify Product in Cart
-        String product = driver.findElement(By.xpath("//td[@class='product-item-name']")).getText();
-        Assert.assertEquals(product, "Overnight Duffle");
+        // Verify Order Summary
+        WebElement orderSummary = driver.findElement(By.xpath("//ol[@id='checkout-summary']"));
+        Assert.assertTrue(orderSummary.getText().contains("Overnight Duffle"));
 
-        // Enter Address and Place Order
-        driver.findElement(By.id("new-address")).click();
-        driver.findElement(By.id("street")).sendKeys("4 South Street");
+        // Enter shipping details
+        driver.findElement(By.id("street_1")).sendKeys("4 South Street");
         driver.findElement(By.id("city")).sendKeys("Texas");
-        driver.findElement(By.id("region")).selectByVisibleText("Texas");
+        new Select(driver.findElement(By.id("region_id"))).selectByVisibleText("Texas");
         driver.findElement(By.id("zip")).sendKeys("77567");
-        driver.findElement(By.id("phone")).sendKeys("3456788765");
-        driver.findElement(By.id("ship-here")).click();
-        driver.findElement(By.id("fixed")).click();
-        driver.findElement(By.id("same-address")).click();
-        driver.findElement(By.id("place-order")).click();
+        driver.findElement(By.id("telephone")).sendKeys("3456788765");
+        driver.findElement(By.id("ship_here")).click();
 
-        // Verify Success Message
-        String successMsg = driver.findElement(By.id("success-msg")).getText();
-        Assert.assertEquals(successMsg, "Thank you for your purchase!");
+        // Select Fixed shipping method and proceed
+        driver.findElement(By.id("s_method_flatrate_flatrate")).click();
+        driver.findElement(By.xpath("//span[text()='Next']")).click();
+
+        // Place Order
+        driver.findElement(By.id("billing:use_for_shipping_yes")).click();
+        driver.findElement(By.xpath("//span[text()='Place Order']")).click();
+
+        // Verify Success Page
+        WebElement successMsg = driver.findElement(By.xpath("//h1[text()='Thank you for your purchase!']"));
+        Assert.assertEquals(successMsg.getText(), "Thank you for your purchase!");
 
         // Sign Out
-        driver.findElement(By.id("change")).click();
-        driver.findElement(By.linkText("Signout")).click();
-
-        tearDown();
-    }
-
-    public static void main(String[] args) {
-        MagentoOrderTest test = new MagentoOrderTest();
-        test.placeOrderTest();
+        driver.findElement(By.linkText("Sign Out")).click();
     }
 }
-```
-
