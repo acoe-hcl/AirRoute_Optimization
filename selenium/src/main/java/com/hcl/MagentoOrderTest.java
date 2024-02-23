9fc567d96850c3552566fa199b7957b61c8b8008
vijay <vijay@10.131.70.154>
2024-02-23 09:57:03 +0000

    Commit all changes including additions

--- selenium/src/main/java/com/hcl/MagentoOrderTest.java
+++ selenium/src/main/java/com/hcl/MagentoOrderTest.java
@@ -1,7 +1,12 @@
+Here's a sample Java Selenium test script for the given scenario.
 
+Please note that the identifiers (id, class, xpath, etc.) for the web elements are assumed based on the name provided in the scenario. You would need to replace them with the actual identifiers from the application's HTML code.
+
+```java
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
@@ -11,8 +16,9 @@ public class MagentoOrderTest {
     WebDriver driver;
 
     @Test
-    public void placeOrderTest() {
-        // Initialize WebDriver
+    public void placeOrder() {
+        System.setProperty("webdriver.chrome.driver", "path to chromedriver");
+        driver = new ChromeDriver();
 
         // Launch the application
         driver.get("https://magento.softwaretestingboard.com/");
@@ -23,45 +29,43 @@ public class MagentoOrderTest {
         driver.findElement(By.id("pass")).sendKeys("Tester@123");
         driver.findElement(By.id("send2")).click();
 
-        // Navigate to Bags - Gear
+        // Navigate to Bags
         Actions actions = new Actions(driver);
-        WebElement gearMenu = driver.findElement(By.xpath("//*[@id='ui-id-5']"));
+        WebElement gearMenu = driver.findElement(By.id("gear"));
         actions.moveToElement(gearMenu).perform();
         driver.findElement(By.linkText("Bags")).click();
 
-        // Select Overnight Duffle
-        driver.findElement(By.xpath("//a[@title='Overnight Duffle']")).click();
-        driver.findElement(By.xpath("//span[text()='Add to Cart']")).click();
+        // Select product
+        driver.findElement(By.id("duffle")).click();
+        driver.findElement(By.id("add-to-cart")).click();
 
-        // Proceed to Checkout
+        // Checkout
         driver.findElement(By.linkText("My Cart")).click();
-        driver.findElement(By.xpath("//span[text()='Proceed to Checkout']")).click();
+        driver.findElement(By.id("proceed-to-checkout")).click();
+        Assert.assertTrue(driver.findElement(By.id("order-summary")).getText().contains("Overnight Duffle"));
 
-        // Verify Order Summary
-        WebElement orderSummary = driver.findElement(By.xpath("//ol[@id='checkout-summary']"));
-        Assert.assertTrue(orderSummary.getText().contains("Overnight Duffle"));
-
-        // Enter shipping details
-        driver.findElement(By.id("street_1")).sendKeys("4 South Street");
+        // Enter address and place order
+        driver.findElement(By.id("new-address")).click();
+        driver.findElement(By.id("street")).sendKeys("4 South Street");
         driver.findElement(By.id("city")).sendKeys("Texas");
-        new Select(driver.findElement(By.id("region_id"))).selectByVisibleText("Texas");
+        new Select(driver.findElement(By.id("state"))).selectByVisibleText("Texas");
         driver.findElement(By.id("zip")).sendKeys("77567");
-        driver.findElement(By.id("telephone")).sendKeys("3456788765");
-        driver.findElement(By.id("ship_here")).click();
+        driver.findElement(By.id("phone")).sendKeys("3456788765");
+        driver.findElement(By.id("ship-here")).click();
+        driver.findElement(By.id("fixed")).click();
+        driver.findElement(By.id("next")).click();
+        driver.findElement(By.id("billing-shipping-same")).click();
+        driver.findElement(By.id("place-order")).click();
 
-        // Select Fixed shipping method and proceed
-        driver.findElement(By.id("s_method_flatrate_flatrate")).click();
-        driver.findElement(By.xpath("//span[text()='Next']")).click();
+        // Verify success message
+        Assert.assertEquals(driver.findElement(By.id("success-message")).getText(), "Thank you for your purchase!");
 
-        // Place Order
-        driver.findElement(By.id("billing:use_for_shipping_yes")).click();
-        driver.findElement(By.xpath("//span[text()='Place Order']")).click();
+        // Sign out
+        driver.findElement(By.linkText("Change")).click();
+        driver.findElement(By.linkText("Signout")).click();
 
-        // Verify Success Page
-        WebElement successMsg = driver.findElement(By.xpath("//h1[text()='Thank you for your purchase!']"));
-        Assert.assertEquals(successMsg.getText(), "Thank you for your purchase!");
-
-        // Sign Out
-        driver.findElement(By.linkText("Sign Out")).click();
+        driver.quit();
     }
 }
+```
+This script uses TestNG for assertions, so make sure to include TestNG library if you want to run this script.
