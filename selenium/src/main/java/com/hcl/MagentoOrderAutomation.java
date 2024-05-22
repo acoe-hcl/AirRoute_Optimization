95e6b98067adb96e877ee149c7b0e36aa5a3a3a2
digitalcoe <ashwinkumar.j@hcl.com>
2024-05-22 15:24:47 +0000

    Commit all changes including additions

--- selenium/src/main/java/com/hcl/MagentoOrderAutomation.java
+++ selenium/src/main/java/com/hcl/MagentoOrderAutomation.java
@@ -1,18 +1,23 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MagentoOrderAutomation {
     public static void main(String[] args) {
-        // Configure webdriver path based on your system
-        System.setProperty("webdriver.chrome.driver", "path_to_chromedriver");
+        // Set ChromeDriver path
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
 
-        // Create an instance of ChromeDriver
+        // Initialize ChromeDriver
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
 
         try {
-            // Step 1: Open the Magento application
+            // Step 1: Launch the application
             driver.get("https://magento.softwaretestingboard.com/");
 
             // Step 2: Click on the "Sign In" link
@@ -20,31 +25,31 @@ public class MagentoOrderAutomation {
             signInLink.click();
 
             // Step 3: Enter email and password
-            WebElement emailField = driver.findElement(By.name("login[username]"));
+            WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("login[username]")));
             emailField.sendKeys("autotest567@gmail.com");
 
             WebElement passwordField = driver.findElement(By.name("login[password]"));
             passwordField.sendKeys("Tester@123");
 
             // Step 4: Click on the "Sign In" button
-            WebElement loginButton = driver.findElement(By.cssSelector("[title='Sign In']"));
-            loginButton.click();
+            WebElement signInButton = driver.findElement(By.id("send2"));
+            signInButton.click();
 
             // Step 5: Mouse hover on the "Gear" menu
-            WebElement gearMenu = driver.findElement(By.className("greeting-link"));
-            Actions action = new Actions(driver);
-            action.moveToElement(gearMenu).perform();
+            WebElement gearMenu = driver.findElement(By.linkText("Gear"));
+            Actions actions = new Actions(driver);
+            actions.moveToElement(gearMenu).perform();
 
             // Step 6: Click on the "Bags" link
             WebElement bagsLink = driver.findElement(By.linkText("Bags"));
             bagsLink.click();
 
             // Step 7: Click on the "Overnight Duffle" image
-            WebElement overnightDuffleImage = driver.findElement(By.xpath("//img[@alt='Overnight Duffle']"));
-            overnightDuffleImage.click();
+            WebElement duffleImage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img[alt='Overnight Duffle']")));
+            duffleImage.click();
 
             // Step 8: Click on the "Add to Cart" button
-            WebElement addToCartButton = driver.findElement(By.id("product-addtocart-button"));
+            WebElement addToCartButton = driver.findElement(By.cssSelector("button[title='Add to Cart']"));
             addToCartButton.click();
 
             // Step 9: Click on the "My Cart" link
@@ -52,16 +57,15 @@ public class MagentoOrderAutomation {
             myCartLink.click();
 
             // Step 10: Click on the "Proceed to Checkout" button
-            WebElement proceedToCheckoutButton = driver.findElement(By.id("top-cart-btn-checkout"));
-            proceedToCheckoutButton.click();
+            WebElement checkoutButton = driver.findElement(By.id("top-cart-btn-checkout"));
+            checkoutButton.click();
 
-            // Step 11: Verify "Order Summary" has "Overnight Duffle" product
-            WebElement orderSummary = driver.findElement(By.className("cart-summary-item-name"));
-            String productName = orderSummary.getText();
-            if (productName.equals("Overnight Duffle")) {
-                System.out.println("Order summary contains correct product");
+            // Step 11: Verify the "Order Summary" has the "Overnight Duffle" product
+            WebElement orderSummary = driver.findElement(By.xpath("//table[@id='checkout-review-table']//a[contains(text(),'Overnight Duffle')]"));
+            if (orderSummary.isDisplayed()) {
+                System.out.println("Order Summary contains Overnight Duffle product - Test Passed");
             } else {
-                System.out.println("Order summary does not contain correct product");
+                System.out.println("Order Summary does not contain Overnight Duffle product - Test Failed");
             }
 
             // Step 12: Click on the "New Address" button
@@ -76,8 +80,7 @@ public class MagentoOrderAutomation {
             cityField.sendKeys("Texas");
 
             WebElement stateDropdown = driver.findElement(By.id("billing:region_id"));
-            Select stateDropdownSelect = new Select(stateDropdown);
-            stateDropdownSelect.selectByVisibleText("Texas");
+            stateDropdown.sendKeys("Texas");
 
             WebElement postalCodeField = driver.findElement(By.id("billing:postcode"));
             postalCodeField.sendKeys("77567");
@@ -86,11 +89,11 @@ public class MagentoOrderAutomation {
             phoneNumberField.sendKeys("3456788765");
 
             // Step 14: Click on the "Ship Here" button
-            WebElement shipHereButton = driver.findElement(By.id("billing-save"));
+            WebElement shipHereButton = driver.findElement(By.id("billing-buttons-container"));
             shipHereButton.click();
 
             // Step 15: Select the "Fixed" radio button
-            WebElement fixedRadioButton = driver.findElement(By.cssSelector("[for='s_method_flatrate_flatrate']"));
+            WebElement fixedRadioButton = driver.findElement(By.id("s_method_flatrate_flatrate"));
             fixedRadioButton.click();
 
             // Step 16: Click on the "Next" button
@@ -106,24 +109,26 @@ public class MagentoOrderAutomation {
             placeOrderButton.click();
 
             // Step 19: Verify the success message
-            WebElement successMessage = driver.findElement(By.className("success-msg"));
-            String message = successMessage.getText();
-            if (message.contains("Thank you for your purchase!")) {
-                System.out.println("Order placed successfully");
+            WebElement successMessage = driver.findElement(By.xpath("//h1[contains(text(),'Thank you for your purchase!')]"));
+            if (successMessage.isDisplayed()) {
+                System.out.println("Order placed successfully - Test Passed");
             } else {
-                System.out.println("Order placement failed");
+                System.out.println("Order placement failed - Test Failed");
             }
 
             // Step 20: Click on the "Change" button
-            WebElement changeButton = driver.findElement(By.id("changeCustomerId"));
+            WebElement changeButton = driver.findElement(By.linkText("Change"));
             changeButton.click();
 
             // Step 21: Click on the "Signout" link
             WebElement signoutLink = driver.findElement(By.linkText("Signout"));
             signoutLink.click();
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             // Close the browser
             driver.quit();
         }
     }
 }
+``
