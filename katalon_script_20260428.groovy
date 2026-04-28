c25d94f4fcca5825fb7d89878f2e7dfa38dd5daf
acoe.hcl <acoe.hcl@users.noreply.github.com>
2026-04-28 12:46:46 +0530

    Add Katalon script — katalon_script_20260428.groovy

--- katalon_script_20260428.groovy
+++ katalon_script_20260428.groovy
@@ -2,7 +2,7 @@ import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords.findWindowsObject
+import static com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
@@ -13,7 +13,6 @@ import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
@@ -21,71 +20,75 @@ import io.cucumber.java.en.Given
 import io.cucumber.java.en.When
 import io.cucumber.java.en.Then
 
-class D365UserSiteSteps {
+class UserSiteFieldsSteps {
 
-    @Given("User login to D365 for {string}")
-    def userLoginToD365(String testcaseName) {
+    @Given("User login to D365 for (.*)")
+    def userLogin(String testcaseName) {
         WebUI.openBrowser('')
-        WebUI.navigateToUrl(GlobalVariable.D365_URL)
-        WebUI.setText(findTestObject('LoginPage/Username'), GlobalVariable.D365_Username)
-        WebUI.setText(findTestObject('LoginPage/Password'), GlobalVariable.D365_Password)
-        WebUI.click(findTestObject('LoginPage/LoginButton'))
-        WebUI.verifyMatch(WebUI.getWindowTitle(), 'Microsoft Dynamics 365', false)
+        WebUI.navigateToUrl('https://d365.yourcompany.com')
+        WebUI.setText(findTestObject('Page_Login/input_Username'), 'Lomas.Gupta')
+        WebUI.setText(findTestObject('Page_Login/input_Password'), 'your_password')
+        WebUI.click(findTestObject('Page_Login/button_Login'))
+        // Optionally verify login
+        WebUI.verifyElementPresent(findTestObject('Page_Home/icon_Home'), 10)
     }
 
-    @Given("User click to select {string}")
-    def userClickToSelectLegalEntity(String legalentity) {
-        WebUI.click(findTestObject('HomePage/LegalEntityDropdown'))
-        WebUI.setText(findTestObject('HomePage/LegalEntitySearchBox'), legalentity)
-        WebUI.sendKeys(findTestObject('HomePage/LegalEntitySearchBox'), Keys.ENTER)
-        WebUI.verifyMatch(WebUI.getText(findTestObject('HomePage/SelectedLegalEntity')), legalentity, false)
+    @Given("User click to select (.*)")
+    def userSelectLegalEntity(String legalentity) {
+        WebUI.click(findTestObject('Page_Home/dropdown_LegalEntity'))
+        WebUI.setText(findTestObject('Page_Home/input_LegalEntity'), legalentity)
+        WebUI.sendKeys(findTestObject('Page_Home/input_LegalEntity'), Keys.chord(Keys.ENTER))
     }
 
     @When("user navigates to the 'User sites' page")
-    def userNavigatesToUserSitesPage() {
-        WebUI.click(findTestObject('HomePage/UserSitesMenu'))
-        WebUI.verifyElementPresent(findTestObject('UserSitesPage/UserSitesHeader'), 10)
+    def navigateToUserSitesPage() {
+        WebUI.click(findTestObject('Page_Home/menu_UserSites'))
+        WebUI.verifyElementPresent(findTestObject('Page_UserSites/title_UserSites'), 10)
     }
 
     @When("user removes all the user sites with user ID 'Lomas.Gupta'")
-    def userRemovesAllUserSites() {
-        // Assuming a table with user sites and a delete button per row
-        while (WebUI.verifyElementPresent(findTestObject('UserSitesPage/UserRow', ['userId' : 'Lomas.Gupta']), 2, FailureHandling.OPTIONAL)) {
-            WebUI.click(findTestObject('UserSitesPage/DeleteButton', ['userId' : 'Lomas.Gupta']))
-            WebUI.acceptAlert()
-            WebUI.delay(1)
+    def removeAllUserSites() {
+        WebUI.setText(findTestObject('Page_UserSites/input_UserID'), 'Lomas.Gupta')
+        WebUI.click(findTestObject('Page_UserSites/button_Search'))
+        while (WebUI.verifyElementPresent(findTestObject('Page_UserSites/row_UserSite'), 2, FailureHandling.OPTIONAL)) {
+            WebUI.click(findTestObject('Page_UserSites/button_Delete'))
+            WebUI.click(findTestObject('Page_UserSites/button_ConfirmDelete'))
         }
     }
 
-    @When("add a user site with LE '{string}', user 'Lomas.gupta', Site '7100' and isDefault 'true' if not already added")
+    @When("add a user site with LE '(.*)', user 'Lomas.gupta', Site '7100' and isDefault 'true' if not already added")
     def addUserSite7100(String legalentity) {
-        if (!WebUI.verifyElementPresent(findTestObject('UserSitesPage/UserSiteRow', ['userId' : 'Lomas.Gupta', 'site' : '7100', 'isDefault' : 'true']), 2, FailureHandling.OPTIONAL)) {
-            WebUI.click(findTestObject('UserSitesPage/AddButton'))
-            WebUI.setText(findTestObject('UserSitesPage/LegalEntityInput'), legalentity)
-            WebUI.setText(findTestObject('UserSitesPage/UserInput'), 'Lomas.gupta')
-            WebUI.setText(findTestObject('UserSitesPage/SiteInput'), '7100')
-            WebUI.click(findTestObject('UserSitesPage/IsDefaultCheckbox'))
-            WebUI.click(findTestObject('UserSitesPage/SaveButton'))
+        if (!WebUI.verifyElementPresent(findTestObject('Page_UserSites/row_Site7100'), 2, FailureHandling.OPTIONAL)) {
+            WebUI.click(findTestObject('Page_UserSites/button_Add'))
+            WebUI.setText(findTestObject('Page_UserSites/input_LE'), legalentity)
+            WebUI.setText(findTestObject('Page_UserSites/input_User'), 'Lomas.gupta')
+            WebUI.setText(findTestObject('Page_UserSites/input_Site'), '7100')
+            WebUI.click(findTestObject('Page_UserSites/checkbox_IsDefault'))
+            WebUI.click(findTestObject('Page_UserSites/button_Save'))
         }
     }
 
-    @When("add a user site with LE '{string}', user 'Lomas.gupta', Site '3434' and isDefault 'true' if not already added")
+    @When("add a user site with LE '(.*)', user 'Lomas.gupta', Site '3434' and isDefault 'true' if not already added")
     def addUserSite3434(String legalentity) {
-        if (!WebUI.verifyElementPresent(findTestObject('UserSitesPage/UserSiteRow', ['userId' : 'Lomas.Gupta', 'site' : '3434', 'isDefault' : 'true']), 2, FailureHandling.OPTIONAL)) {
-            WebUI.click(findTestObject('UserSitesPage/AddButton'))
-            WebUI.setText(findTestObject('UserSitesPage/LegalEntityInput'), legalentity)
-            WebUI.setText(findTestObject('UserSitesPage/UserInput'), 'Lomas.gupta')
-            WebUI.setText(findTestObject('UserSitesPage/SiteInput'), '3434')
-            WebUI.click(findTestObject('UserSitesPage/IsDefaultCheckbox'))
-            WebUI.click(findTestObject('UserSitesPage/SaveButton'))
+        if (!WebUI.verifyElementPresent(findTestObject('Page_UserSites/row_Site3434'), 2, FailureHandling.OPTIONAL)) {
+            WebUI.click(findTestObject('Page_UserSites/button_Add'))
+            WebUI.setText(findTestObject('Page_UserSites/input_LE'), legalentity)
+            WebUI.setText(findTestObject('Page_UserSites/input_User'), 'Lomas.gupta')
+            WebUI.setText(findTestObject('Page_UserSites/input_Site'), '3434')
+            WebUI.click(findTestObject('Page_UserSites/checkbox_IsDefault'))
+            WebUI.click(findTestObject('Page_UserSites/button_Save'))
         }
     }
 
     @When("test for the validation error message 'User cannot have multiple default sites in same legal entity' on the Sales Order page")
-    def verifyValidationErrorMessage() {
-        WebUI.click(findTestObject('UserSitesPage/NavigateToSalesOrder'))
-        WebUI.verifyElementPresent(findTestObject('SalesOrderPage/ErrorMessage'), 10)
-        WebUI.verifyMatch(WebUI.getText(findTestObject('SalesOrderPage/ErrorMessage')), 
-            'User cannot have multiple default sites in same legal entity', false)
+    def validateErrorMessage() {
+        WebUI.click(findTestObject('Page_SalesOrder/menu_SalesOrder'))
+        WebUI.verifyElementPresent(findTestObject('Page_SalesOrder/label_Error'), 10)
+        String actualError = WebUI.getText(findTestObject('Page_SalesOrder/label_Error'))
+        WebUI.verifyMatch(actualError, 'User cannot have multiple default sites in same legal entity', false)
+        WebUI.closeBrowser()
     }
 }
+
+// Test Suite/Runner Example
+CucumberKW.runWithCucumberRunner(UserSiteFieldsSteps)
